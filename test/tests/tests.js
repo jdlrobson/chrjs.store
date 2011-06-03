11deac66934da809dea42f4cbbdf8d9b409e9e67
Jon Robson <jdlrobson@gmail.com>
2011-06-03 11:36:58 +0100

    added tests for getTiddler / addTiddler methods

--- test/tests/tests.js
+++ test/tests/tests.js
@@ -7,5 +7,26 @@ module("chrjs.store", {
 	}
 });
 
+test("getTiddler / addTiddler (tiddler object)", function() {
+	var store = new tiddlyweb.Store();
+	var test = store.getTiddler("test");
+	var tiddler = new tiddlyweb.Tiddler("test");
+	store.addTiddler(tiddler);
+	var test2 = store.getTiddler("test");
+
+	strictEqual(test, null, "no tiddler called test initially in store");
+	strictEqual(test2.title, "test", "check the returned tiddler has title test");
+});
+
+test("getTiddler / addTiddler (json)", function() {
+	var store = new tiddlyweb.Store();
+	var test = store.getTiddler("test");
+	store.addTiddler({ title: "test", fields: { foo: "bar" } });
+	var test2 = store.getTiddler("test");
+
+	strictEqual(test, null, "no tiddler called test initially in store");
+	strictEqual(test2.title, "test", "check the returned tiddler has title test");
+	strictEqual(test2.fields.foo, "bar", "the field was set on the new tiddler");
+});
 
 })(QUnit.module, jQuery);

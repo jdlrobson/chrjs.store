e4c3ba5c375147f3f61bb74e917e0326a66152fd
Jon Robson <jdlrobson@gmail.com>
2011-06-03 11:56:23 +0100

    added each tests
    this provides basic coverage for the each function

--- test/tests/tests.js
+++ test/tests/tests.js
@@ -1,9 +1,16 @@
 (function(module, $) {
 
+var testStore;
 module("chrjs.store", {
 	setup: function() {
+		testStore = new tiddlyweb.Store();
+		testStore.addTiddler({ title: "abc", tags: [] });
+		testStore.addTiddler({ title: "def", tags: [] });
+		testStore.addTiddler({ title: "ghi", tags: ["foo"] });
+		testStore.addTiddler({ title: "jkl", tags: ["foo"] });
 	},
 	teardown: function() {
+		testStore = null;
 	}
 });
 
@@ -27,6 +34,16 @@ test("getTiddler / addTiddler (json)", function() {
 	strictEqual(test, null, "no tiddler called test initially in store");
 	strictEqual(test2.title, "test", "check the returned tiddler has title test");
 	strictEqual(test2.fields.foo, "bar", "the field was set on the new tiddler");
+});
+
+test("each", function() {
+	var test = [];
+	var tiddlers = testStore.each(function(tiddler) {
+		if(tiddler.tags.indexOf("foo") > -1) {
+			test.push(tiddler);
+		}
+	});
+	strictEqual(tiddlers.length, 2, "only 2 tiddlers are tagged foo");
 });
 
 })(QUnit.module, jQuery);
